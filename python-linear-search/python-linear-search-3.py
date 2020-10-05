649a1f1be03dc818ed1743a64f67f4081999ea09
mukulsrmcse <44325121+mukulsrmcse@users.noreply.github.com>
2020-10-06 00:31:28 +0530

    Created python-linear-search-3.py

---
+++ python-linear-search/python-linear-search-3.py
@@ -0,0 +1,14 @@
+def linearsearch(arr, x):
+   for i in range(len(arr)):
+      if arr[i] == x:
+         return i
+   return "Not found"
+arr = ['t','u','t','o','r','i','a','l']
+x = '5'
+
+if str(linearsearch(arr,x)) == 'Not found':
+
+    print("Element not found")
+    
+else:
+    print("Element found at index "+str(linearsearch(arr,x)))
