d73f9ddc8aa575d2e6bad0b33dc680882973bf2a
mukulsrmcse <44325121+mukulsrmcse@users.noreply.github.com>
2020-10-06 00:43:19 +0530

    Added Python-Linear-Search

--- python-linear-search/python-linear-search-3.py
+++ python-linear-search/python-linear-search-3.py
@@ -1,14 +1,20 @@
+arr = list() #Enter elements
+e = raw_input("Enter how many elements you want: ")
+print "Enter numbers in array: "
+
+for i in range(int(e)):
+    n = raw_input("e: ")
+    arr.append(int(n))
+
+x = raw_input("Element to find: ") 
+
 def linearsearch(arr, x):
    for i in range(len(arr)):
       if arr[i] == x:
          return i
    return "Not found"
-arr = ['t','u','t','o','r','i','a','l']
-x = '5'
 
 if str(linearsearch(arr,x)) == 'Not found':
-
-    print("Element not found")
-    
+    print("Element not found")    
 else:
     print("Element found at index "+str(linearsearch(arr,x)))
